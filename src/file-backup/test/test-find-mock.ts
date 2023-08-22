7d21fdc0551fd16094d40ce81b6be928e6ab5c26
bzy-debug <bzy_sustech@foxmail.com>
2023-08-22 23:55:57 +0800

    Add mock test

--- src/file-backup/test/test-find-mock.ts
+++ src/file-backup/test/test-find-mock.ts
@@ -30,41 +30,41 @@ describe('checks for pre-existing hashes using mock filesystem', () => {
 
   // [tests]
   it('finds no pre-existing files when none given or exist', async () => {
-    const expected = {}
+    const expected = new Map(Object.entries({}))
     const actual = await findNew('bck-0-csv-0', [])
     assert.deepStrictEqual(expected, actual,
       'Expected no files')
   })
 
   it('finds some files when one file is given and none exist', async () => {
-    const candidates = [['somefile.txt', '9876fedc']]
-    const expected = { '9876fedc': 'somefile.txt' }
+    const candidates: Array<[string, string]> = [['somefile.txt', '9876fedc']]
+    const expected = new Map(Object.entries({ '9876fedc': 'somefile.txt' }))
     const actual = await findNew('bck-0-csv-0', candidates)
     assert.deepStrictEqual(expected, actual,
       'Expected one file')
   })
 
   it('finds nothing needs backup when there is a match', async () => {
-    const candidates = [['alpha.js', 'abcd1234']]
-    const expected = {}
+    const candidates: Array<[string, string]> = [['alpha.js', 'abcd1234']]
+    const expected = new Map(Object.entries({}))
     const actual = await findNew('bck-1-csv-1', candidates)
     assert.deepStrictEqual(expected, actual,
       'Expected no files')
   })
 
   it('finds something needs backup when there is a mismatch', async () => {
-    const candidates = [['alpha.js', 'a1b2c3d4']]
-    const expected = { a1b2c3d4: 'alpha.js' }
+    const candidates: Array<[string, string]> = [['alpha.js', 'a1b2c3d4']]
+    const expected = new Map(Object.entries({ a1b2c3d4: 'alpha.js' }))
     const actual = await findNew('bck-1-csv-1', candidates)
     assert.deepStrictEqual(expected, actual,
       'Expected one file')
   })
 
   it('finds mixed matches', async () => {
-    const candidates = [['matches.js', '3456cdef'],
+    const candidates: Array<[string, string]> = [['matches.js', '3456cdef'],
       ['matches.txt', 'abcd1234'],
       ['mismatch.txt', '12345678']]
-    const expected = { 12345678: 'mismatch.txt' }
+    const expected = new Map(Object.entries({ 12345678: 'mismatch.txt' }))
     const actual = await findNew('bck-4-csv-2', candidates)
     assert.deepStrictEqual(expected, actual,
       'Expected one file')
